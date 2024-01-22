f4151016b1da20dd2725aeca0a53b38fec510d50
Ajay-Patel009 <ajay.patel@appinventiv.com>
2024-01-22 18:08:31 +0530

    removing db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,10 @@ import { ProductModule } from './product/product.module'; // 2.1 Import the prod
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost/store'), // 1.2 Setup the database
-    ProductModule, UserModule, // 2.2 Add the product module
-  ],
+  // imports: [
+  //   MongooseModule.forRoot('mongodb://localhost/store'), // 1.2 Setup the database
+  //   ProductModule, UserModule, // 2.2 Add the product module
+  // ],
   controllers: [AppController],
   providers: [AppService],
 })
